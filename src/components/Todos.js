18fa992e9737bf405e47da9a720cb6debb1118c3
ryanyuri357 <ryan.yuri357@gmail.com>
2021-08-02 19:09:39 -0500

    Fixed todo done

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,28 +4,29 @@ import { FaCheckDouble } from "react-icons/fa";
 import { TodoContext } from "../context/TodoContext";
 import { REMOVE_TODO } from "../context/action.types";
 
+
 const Todos = () => {
   const { todos, dispatch } = useContext(TodoContext);
 
   return (
-    <ListGroup className="mt-5 mb-2 items">
-      {todos.map(todo => (
-        <ListGroupItem key={todo.id}>
-          {todo.todoString}
-          <span
-            className="float-right"
-            onClick={() => {
-              dispatch({
-                type: REMOVE_TODO,
-                payload: todo.id
-              });
-            }}
-          >
-            <FaCheckDouble />
-          </span>
-        </ListGroupItem>
-      ))}
-    </ListGroup>
+      <ListGroup className="mt-5 mb-2 items">
+        {todos.map(todo => (
+            <ListGroupItem key={todo.id}>
+              {todo.todoString}
+              <span
+                  className="float-end"
+                  onClick={() => {
+                    dispatch({
+                      type: REMOVE_TODO,
+                      payload: todo.id
+                    });
+                  }}
+              >
+                <FaCheckDouble />
+              </span>
+            </ListGroupItem>
+        ))}
+      </ListGroup>
   );
 };
 
